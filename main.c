61cea871890cb8eb6caec988af89d85384079322
Leonardo Moreira <moreira.leonardo83@oulook.com>
2023-05-16 15:48:26 -0300

    Menu
    Criação da função print do menu e dos possíveis switch case

--- main.c
+++ main.c
@@ -1,15 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <locale.h>
+
+void print_menu(){
+
+    printf("-------------------MENU--------------------\n");
+    printf("1 - Cadastrar medico\n");
+    printf("2 - Remover medico\n");
+    printf("3 - Buscar medico\n");
+    printf("4 - Numero de medicos cadastrados\n");
+    printf("5 - Sair\n");
+    printf("-------------------------------------------\n");
+
+}
 
 int main(){
     
-    printf("Teste\n");
-    printf("Virtim testando\n");
-    printf("Galo");
-    printf("gd");
+    int option;
 
-    setlocale(LC_ALL,NULL);
+    do{
+        print_menu();
+        printf("Insira uma opcao: ");
+        scanf("%d",&option);
+
+        switch(option){
+
+            case 1:
+                break;
+            
+            case 2:
+                break;
+
+            case 3:
+                break;
+            
+            case 4:
+                break;
+            
+            case 5:
+                break;
+            default:
+                printf("Erro!!\nOpcao selecionada nao existe. Insira novamente!!\n");
+
+        }
+
+    }while(option != 5);
 
     return 0;
 }
